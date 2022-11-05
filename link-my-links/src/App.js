35d4f34abf55882bce33a0cb30a5d4106cd15227
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-11-06 05:12:56 +0530

    Added Basic Routes

--- link-my-links/src/App.js
+++ link-my-links/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Lists from './components/Lists/Lists';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hiii</h1>
+      <Router>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/lists" element={<Lists />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ link-my-links/src/components/Lists/Lists.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lists = () => {
+  return (
+    <div>Lists</div>
+  )
+}
+
+export default Lists
