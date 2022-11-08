6e09317d4e76e67e7664420c5ca5fae7fc455c52
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-11-08 12:31:29 +0530

    Added Greet Model

---
+++ client/src/components/Greet/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+    <div className='d-none'>
+        <h1>Hello Saket Nice to See You! :)</h1>
+    </div>
+  )
+}
+
+export default Greet
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AddLink from "../AddLink/AddLink";
+import Greet from '../Greet/Greet';
 import Links from '../Links/Links';
 
 const Home = () => {
   return (
     <div>
+      <Greet />
       <Links />
       <AddLink />
       </div>
