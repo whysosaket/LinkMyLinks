cf0262fec29b155cffdc6644b2b7450e3a135a27
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-11-08 12:50:57 +0530

    Added Links Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import About from './components/About/About';
-import Home from './components/Home/Home';
-import Lists from './components/Lists/Lists';
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Lists from "./components/Lists/Lists";
 import Login from "./components/Login/Login";
 import Footer from "./components/Footer/Footer";
-
+import LinkState from "./context/links/LinkState";
+import AlertState from "./context/alerts/AlertState";
+import Alert from "./components/Alert/Alert";
 
 function App() {
-  
   return (
-      <Router>
-        <div className="mainContainer">
-        <Navbar />
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route exact path="/lists" element={<Lists />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-          </Routes>
-        </div>
-        <Footer />
-        </div>
-      </Router>
+    <>
+      <LinkState>
+        <AlertState>
+          <Router>
+            <div className="mainContainer">
+              <Navbar />
+              <Alert />
+              <div>
+                <Routes>
+                  <Route exact path="/" element={<Home />}></Route>
+                  <Route exact path="/lists" element={<Lists />}></Route>
+                  <Route path="/about" element={<About />}></Route>
+                  <Route path="/login" element={<Login />}></Route>
+                </Routes>
+              </div>
+              <Footer />
+            </div>
+          </Router>
+        </AlertState>
+      </LinkState>
+    </>
   );
 }
 
---
+++ client/src/context/links/LinkState.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import LinkContext from "./linkContext";
+
+const LinkState = (props) => {
+  const host = "http://192.168.29.73:9000";
+
+  const [links, setLinks] = useState([]);
+
+  // Get all links
+  const getLinks = async () => {
+    // Doing a API CALL
+    const response = await fetch(`${host}/api/link/fetchalllinks`, {
+      method: 'GET', 
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      }
+    });
+    const json = await response.json();
+    setLinks(json);
+  };
+
+
+  // Add a new Link
+  const addLink = async (title, linkaddress, list, isPublic) => {
+    // Doing a API CALL
+    await fetch(`${host}/api/links/addnote`, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      },
+      body: JSON.stringify({title, linkaddress, list, public: isPublic})
+    });
+    
+    getLinks();
+  };
+
+  // Delete a Link
+  const deleteLink = async (id) => {
+    // TODO - Delete note in backend aswell
+    await fetch(`${host}/api/api/link/${id}`, {
+      method: 'DELETE', 
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      }
+    });
+    getLinks();
+  };
+
+  // Edit a note
+  const editLink = async (_id, title, linkaddress, list, isPublic) => {
+    // API call
+    await fetch(`${host}/api/link/updatelink/${_id}`, {
+      method: 'PUT', 
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      },
+      body: JSON.stringify({title, linkaddress, list, public: isPublic}) 
+    });
+
+
+
+    //logic to edit in client
+    for (let i = 0; i < links.length; i++) {
+      const element = links[i];
+      
+      if(element._id===_id){
+        element.title = title;
+        element.linkaddress = linkaddress;
+        element.list = list;
+        element.isPublic = isPublic;
+      }
+    }
+    getLinks();
+  };
+
+  return (
+    <LinkContext.Provider value={{ links, addLink, deleteLink, editLink, getLinks }}>
+      {props.children}
+    </LinkContext.Provider>
+  );
+};
+
+export default LinkState;
