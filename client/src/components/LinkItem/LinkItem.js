3b380f0d6f414da9da4e63913bcbadfe5285bf4f
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-11-09 01:58:07 +0530

    Added Delete Option

--- client/src/components/LinkItem/LinkItem.js
+++ client/src/components/LinkItem/LinkItem.js
@@ -6,6 +6,11 @@ const LinkItem = (props) => {
 
   const {_id, title, linkaddress, list } = props.link
 
+  const handleDelete = ()=>{
+    props.deleteLink(_id);
+    console.log('clicked');
+  }
+
   return (
       <div className="link-item">
         <div className="info">
@@ -16,7 +21,9 @@ const LinkItem = (props) => {
         <div className="buttons-section">
             <Button text="Open" />
           <button className="space" style={{visibility: "hidden"}}>.</button>
+          <span onClick={handleDelete}>
           <i className="fa-solid fa-xmark fa-2xl space cross"></i>
+          </span>
           <i className="fa-solid fa-pen fa-xl space edit"></i>
         </div>
       </div>
