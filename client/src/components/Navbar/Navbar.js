6d1f968deaff33e079908c5dac310201071e0965
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-04-24 12:19:31 +0530

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link
