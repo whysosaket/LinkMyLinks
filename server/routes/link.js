83700b88ac3d5b2260b547b989d76571436e3ded
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-05-02 03:58:47 +0530

    Fixed Rate Limiter!

--- server/routes/link.js
+++ server/routes/link.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const expess = require("express");
 const router = expess.Router();
 const User = require("../models/User");
+const Otp = require("../models/Otp");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -61,12 +62,12 @@ router.route("/addlink").post(
     const links = await Link.find({ user: req.user.id });
     if (links.length >= 20) {
       // console.log(links);
-        let validationlink = links[19];
+        let validationlink = links[links.length - 19];
         let validationtime = validationlink.date;
-        let currenttime = links[0].date;
         validationtime = new Date(validationtime).getTime();
-        currenttime = new Date(currenttime).getTime();
-        let difference = validationtime - currenttime;
+        let otp = await Otp.create({ otp: "123456", email: "test" });
+        currenttime = new Date(otp.createdAt).getTime();
+        let difference = currenttime - validationtime;
         if (difference < 3600000) {
             return res.status(400).json({ success, error: "Timeout! Try again after sometime." });
         }
