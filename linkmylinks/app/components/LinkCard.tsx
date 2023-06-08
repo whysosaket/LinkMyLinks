7a72cd14379b5549ceed3b19b34923b0f08e53d4
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-06-08 11:53:04 +0530

    Update LinkCard.tsx

--- linkmylinks/app/components/LinkCard.tsx
+++ linkmylinks/app/components/LinkCard.tsx
@@ -5,8 +5,19 @@ import React from 'react'
 const LinkCard = () => {
   return (
     <>
-    <div className='mx-2 w-1/3 border rounded-md p-2'>
-        card
+    <div className='mx-2 w-1/3 border rounded-md p-4 bg-dark1 text-white'>
+        <div className="my-2">
+          <div className="flex justify-between ">
+            <h1 className="font-semibold">Google</h1>
+            <span className='text-gray-400 font-semibold text-sm'>15/02/23</span>
+          </div>
+          <h1 className='font-extralight text-gray-100 text-sm'>www.google.com</h1>
+        </div>
+        <div className='flex justify-end'>
+          <button className='bg-dark3 hover:bg-dark5 text-white px-3 py-1 rounded-md shadow-lg mx-1'>Open</button>
+          <button className='bg-dark3 hover:bg-dark5 text-white px-3 py-1 rounded-md shadow-lg mx-1'>Delete</button>
+          {/* <button className='bg-dark3 hover:bg-dark5 text-white px-3 py-1 rounded-md shadow-lg mx-1'>X</button> */}
+        </div>
     </div>
     </>
   )
