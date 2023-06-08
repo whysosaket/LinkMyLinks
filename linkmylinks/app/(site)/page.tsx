67ee135fc963db5c1a954853006d335a7a014330
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-06-08 09:20:30 +0530

    Initial NextJs Commit

---
+++ linkmylinks/app/(site)/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="font-semibold text-red-400 text-center">Hello World</h1>
+    </>
+  )
+}
